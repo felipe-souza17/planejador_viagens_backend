b0ea1bea5ec2ac29e09f5e7a511150bddf2d2e9c
felipe-souza17 <felipesantos.rsouza@gmail.com>
2024-07-09 17:08:58 -0300

    test: testes adicionado e commitados

---
+++ src/models/repositories/links_repository_test.py
@@ -0,0 +1,29 @@
+import pytest
+import uuid
+from .links_repository import LinksRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
+db_connection_handler.connect()
+trip_id = str(uuid.uuid4())
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_registry_link():
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksRepository(conn)
+
+    link_infos = {
+        "id": str(uuid.uuid4()),
+        "trip_id": trip_id,
+        "link": "www.link.com.br"
+    }
+
+    links_repository.registry_link(link_infos)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_find_links_from_trip():
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksRepository(conn)
+
+    links = links_repository.find_links_from_trip(trip_id)
+    print()
+    print(links)
