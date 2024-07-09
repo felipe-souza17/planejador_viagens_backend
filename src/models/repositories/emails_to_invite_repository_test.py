c350f6849df71cfa6820bead8485fe093da9b148
felipe-souza17 <felipesantos.rsouza@gmail.com>
2024-07-09 12:16:34 -0300

    test: testes com o emails adicionado

---
+++ src/models/repositories/emails_to_invite_repository_test.py
@@ -0,0 +1,26 @@
+import pytest
+import uuid
+from .emails_to_invite_repository import EmailsToInviteRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
+db_connection_handler.connect()
+trip_id = str(uuid.uuid4())
+
+def test_registry_email():
+    conn = db_connection_handler.get_connection()
+    emails_to_invite_repository = EmailsToInviteRepository(conn)
+
+    email_trips_infos = {
+        "id": str(uuid.uuid4()),
+        "trip_id": trip_id,
+        "email": "postman@email.com"
+    }
+
+    emails_to_invite_repository.registry_email(email_trips_infos)
+def test_find_emails_from_trip():
+    conn = db_connection_handler.get_connection()
+    emails_to_invite_repository = EmailsToInviteRepository(conn)
+
+    emails = emails_to_invite_repository.find_emails_from_trip(trip_id)
+    print()
+    print(emails)
